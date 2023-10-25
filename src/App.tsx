99ef4a8eb6fa22c6968eb49fa284fe974d73f607
Jawad <creatorjawad@gmail.com>
2023-10-25 19:03:20 +0500

    File and folder structure done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
---
+++ src/config/constants.ts
@@ -0,0 +1,9 @@
+export const STATUS_CONSTANTS: any = {
+  ERROR:  'error',
+  SUCCESS: 'success',
+  WARNING: 'warning',
+  ACTIVE: 'active',
+  PENDING: 'pending',
+  CLOSED: 'closed',
+  REJECTED: 'rejected',
+};
