59b67156f7ed9ff01929fe49c461d61eab1c154e
Jawad <creatorjawad@gmail.com>
2023-10-25 18:19:42 +0500

    libraries added in pkg.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='font-bold underline'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
