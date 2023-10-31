d191f7975fe046b0e022cb4dc96922e9255cb872
Muhammad Arslan <marslanmustafa391@gmail.com>
2023-10-31 12:22:40 +0500

    api started

--- src/components/BgBox.tsx
+++ src/components/BgBox.tsx
@@ -3,10 +3,11 @@ import React  from 'react';
 interface BgBoxStyle {
   style: React.CSSProperties;
 }
+let boxNo = 1
 
 const BgBox = ({ style}:BgBoxStyle) => {
   return (
-    <div style={style}>
+    <div className={`$box`} style={style}>
     </div>
   );
 };
